aae60a2704229b974e64566d752fbd77ee0ce1c8
NeuroKaran <karanpras321@gmail.com>
2026-03-27 14:47:31 +0530

    chiore: updating the designs

--- script.js
+++ script.js
@@ -40,7 +40,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("animate");
-      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("animate");
     }
   });
 }, observerOptions);
